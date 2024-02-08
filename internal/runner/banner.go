7cc8e420923d55c2461faeaf326c8360b131b331
sandeep <8293321+ehsandeep@users.noreply.github.com>
2024-02-08 20:24:43 +0530

    version update

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -14,7 +14,7 @@ const banner = `
 `
 
 // Version is the current version
-const version = `v0.0.12`
+const version = `v1.0.0`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
